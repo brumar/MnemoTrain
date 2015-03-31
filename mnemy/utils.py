31212e8d6025f727845f0118cf1437c9dc065472
brumar <martin.bruno.mail@gmail.com>
2015-03-31 21:50:30 +0200

    better file opening for windows

--- mnemy/utils.py
+++ mnemy/utils.py
@@ -1,12 +1,15 @@
 import subprocess, os,sys
 
 def openFileMultipleOs(filepath):
+    filepathWindows=filepath.replace("/","\\\\")
+    #script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+    #abs_file_path = os.path.join(script_dir, filepath)
     print("we are trying to open %s for you, if nothing happen, open it manually"%filepath)
     try:
         if sys.platform.startswith('darwin'):#mac
             subprocess.call(('open', filepath))
         elif os.name == 'nt': #windows
-            os.startfile(filepath)
+            os.startfile(filepathWindows)
         elif os.name == 'posix':#linux
             subprocess.call(('xdg-open', filepath))
     except:
